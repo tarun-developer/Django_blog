f9c2ee7e5de169c46480b947fb571f32231bed57
tarun-developer <tkhandagre910@gmail.com>
2018-09-18 22:08:26 +0530

    fiorst

--- polls/urls.py
+++ polls/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [  
-     path('', views.index,name='index'),
-     path('contact/', views.contact,name='contact'),
-      
-     ]
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+
+]
